0f583b569a744b19785914b93df89ee311a1c7b6
fdodino <fernando.dodino@gmail.com>
2018-06-11 23:54:52 -0300

    Forcing travis build

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
 import React from 'react'
-
 import 'jest-enzyme'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { shallow } from 'enzyme'
-
 import App from './App'
 
 configure({ adapter: new Adapter() })
