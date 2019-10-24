e9a22d673fce08f79c549a51fa68798109b271e2
Fernando Dodino <fernando.dodino@gmail.com>
2019-10-24 09:31:03 -0300

    Adding linter - new dependencies (2019 version)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Conversor from './conversor.js'
 import logo from './logo.svg'
 import './App.css'
@@ -6,10 +6,12 @@ import './App.css'
 class App extends Component {
   constructor() {
     super()
-    this.state = { kilometros: "<Ingrese millas>" }
+    this.state = {
+      kilometros: '<Ingrese millas>'
+    }
     //this.convertir = this.convertir.bind(this)
   }
-  
+
   convertir(event) {
     this.setState({
       kilometros: new Conversor().convertir(event.target.value)
@@ -20,15 +22,26 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Conversor <small>React JS</small></h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1>Conversor
+            <small>React JS</small>
+          </h1>
         </div>
         <p>Ingrese millas:</p>
-        <input type="text" name="millas" id="millas" onChange={this.convertir.bind(this)} />
+        <input
+          type="text"
+          name="millas"
+          id="millas"
+          onChange={this
+          .convertir
+          .bind(this)}/>
         <p>Ingrese kilómetros:</p>
-        <p id="kms">{this.state.kilometros.toLocaleString('es')}</p>
+        <p id="kms">{this
+            .state
+            .kilometros
+            .toLocaleString('es')}</p>
       </div>
-    );
+    )
   }
 }
 
