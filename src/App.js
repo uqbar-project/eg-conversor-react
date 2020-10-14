4de6aeef1bc724a6641ee94b85aa05b791e2c383
Fernando Dodino <fernando.dodino@gmail.com>
2020-10-14 09:01:13 -0300

    Using hooks with 2020 version

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
-import { Component } from 'react'
 import React from 'react'
+import { useState } from 'react'
 import { Box, Form, Heading, Tag } from 'react-bulma-components'
 
 import { convertirMillasAKms } from './conversor'
 
 const { Control, Field, Label, Input } = Form
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      millas: '',
-      kilometros: '<Ingrese millas>',
-      colorConversion: 'warning',
-    }
-  }
+const INITIAL_VALUE = ''
 
-  convertir(newMillas) {
-    this.setState({
-      millas: newMillas,
-      kilometros: isNaN(newMillas) ? '<Ingrese un valor numérico>' : convertirMillasAKms(newMillas),
-      colorConversion: isNaN(newMillas) ? 'warning' : 'success',
-    })
-  }
+const App = () => {
+  const [millas, setMillas] = useState(INITIAL_VALUE)
 
-  render() {
-    return (
-      <div className="App">
-        <Box>
-          <Heading>
-            Conversor de millas a kilómetros - React
+  const kilometros = millas === INITIAL_VALUE ? '<Ingrese millas>' : (isNaN(millas) ? '<Ingrese un valor numérico>' : convertirMillasAKms(millas))
+  const colorConversion = millas === INITIAL_VALUE || isNaN(millas) ? 'warning' : 'success'
+
+  return (
+    <div className="App">
+      <Box>
+        <Heading>
+          Conversor de millas a kilómetros - React
         </Heading>
-          <Field>
-            <Label>Millas</Label>
-            <Control>
-              <Input value={this.state.millas} name="millas" autoComplete="off" data-testid="millas" onChange={(event) => this.convertir(event.target.value)} />
-            </Control>
-          </Field>
-          <Field>
-            <Label>Kilómetros</Label>
-            <Tag color={this.state.colorConversion} rounded>
-              <Label data-testid="kms">{this.state.kilometros.toLocaleString('es')}</Label>
-            </Tag>
-          </Field>
-        </Box>
-      </div>
-    )
-  }
+        <Field>
+          <Label>Millas</Label>
+          <Control>
+            <Input value={millas} name="millas" autoComplete="off" data-testid="millas" onChange={(event) => setMillas(event.target.value)} />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Kilómetros</Label>
+          <Tag color={colorConversion} rounded>
+            <Label data-testid="kms">{kilometros.toLocaleString('es')}</Label>
+          </Tag>
+        </Field>
+      </Box>
+    </div>
+  )
 }
 
 export default App
