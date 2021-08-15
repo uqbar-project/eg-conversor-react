71966502c5aa277c3ef7f623a547aea830921b9b
Fernando Dodino <fernando.dodino@gmail.com>
2021-08-15 10:11:53 -0300

    UserEvent explained

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
-import { fireEvent, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
-
 import App from './App'
+
 
 test('convierte un valor > 0 de millas a kilómetros correctamente', async () => {
   const { getByTestId } = render(<App />)
   // El usuario carga 10 en millas
   const inputMillas = getByTestId('millas')
-  fireEvent.change(inputMillas, { target: { value: '10' } })
+  // Una forma de simular la carga del usuario es lanzar manualmente un evento con la información
+  // que recibiría el componente de React
+  // fireEvent.change(inputMillas, { target: { value: '10' } })
+  // una variante más declarativa es
+  userEvent.type(inputMillas, '10')
   // https://stackoverflow.com/questions/52618569/set-the-locale-for-date-prototype-tolocalestring-for-jest-tests
   expect(getByTestId('kms')).toHaveTextContent('16,093')
 })
@@ -20,6 +25,6 @@ test('inicialmente pide que convirtamos de millas a kilómetros', async () => {
 test('si ingresa un valor alfabético la conversión de millas a kilómetros no se realiza', async () => {
   const { getByTestId } = render(<App />)
   const inputMillas = getByTestId('millas')
-  fireEvent.change(inputMillas, { target: { value: 'dos' } })
+  userEvent.type(inputMillas, 'dos')
   expect(getByTestId('kms')).toHaveTextContent('<Ingrese un valor numérico>')
 })
