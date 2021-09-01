3df5e0dd3a69079e9d99fbe919549e51dc02005b
Fernando Dodino <fernando.dodino@gmail.com>
2021-09-01 07:54:11 -0300

    Fix build: using cypress script

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,30 @@
-import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
-
 import App from './App'
 
-configure({ adapter: new Adapter() })
 
-const MILLAS_SELECTOR = '[data-testid="millas"]'
-const KMS_SELECTOR = '[data-testid="kms"]'
+test('convierte un valor > 0 de millas a kilómetros correctamente', async () => {
+  const { getByTestId } = render(<App />)
+  // El usuario carga 10 en millas
+  const inputMillas = getByTestId('millas')
+  // Una forma de simular la carga del usuario es lanzar manualmente un evento con la información
+  // que recibiría el componente de React
+  // fireEvent.change(inputMillas, { target: { value: '10' } })
+  // una variante más declarativa es
+  userEvent.type(inputMillas, '10')
+  // https://stackoverflow.com/questions/52618569/set-the-locale-for-date-prototype-tolocalestring-for-jest-tests
+  expect(getByTestId('kms')).toHaveTextContent('16,093')
+})
 
-it('App levanta', () => {
-  shallow(<App />)
+test('inicialmente pide que convirtamos de millas a kilómetros', async () => {
+  const { getByTestId } = render(<App />)
+  expect(getByTestId('kms')).toHaveTextContent('<Ingrese millas>')
 })
-it('convertir millas a kilómetros - inicialmente pide que ingreses millas', () => {
-  const wrapper = shallow(<App />)
-  const kms = wrapper.find(KMS_SELECTOR)
-  expect(kms.text()).toBe('<Ingrese millas>')
-})
-it('convertir un valor común de millas a kilómetros - convierte correctamente', () => {
-  const wrapper = shallow(<App />)
-  const millas = wrapper.find(MILLAS_SELECTOR)
-  millas.simulate('change', { 'target': { value: '10' } })
-  const kms = wrapper.find(KMS_SELECTOR)
-  expect(kms.text()).toBe('16,093')
+
+test('si ingresa un valor alfabético la conversión de millas a kilómetros no se realiza', async () => {
+  const { getByTestId } = render(<App />)
+  const inputMillas = getByTestId('millas')
+  userEvent.type(inputMillas, 'dos')
+  expect(getByTestId('kms')).toHaveTextContent('<Ingrese un valor numérico>')
 })
