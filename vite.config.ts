749912b2d2b218375fb9143a0f60b58a9aa51b98
Fernando Dodino <fernando.dodino@gmail.com>
2024-07-28 09:55:03 -0300

    Deleting setupTests

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ["./setupTests.ts"],
     coverage: {
       reporter: ['lcov', 'json', 'html', 'json-summary'],
     },
