ea262a7b6c070eba52bf4425fd8e44ad6cf51aa9
fdodino <fernando.dodino@gmail.com>
2025-08-31 21:27:22 -0300

    Using React 19

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,13 +6,8 @@ import { expect, test } from 'vitest'
 
 test('convierte un valor > 0 de millas a kilómetros correctamente', async () => {
   render(<App />)
-  // El usuario carga 10 en millas
   const inputMillas = screen.getByTestId('millas')
-  // Una forma de simular la carga del usuario es lanzar manualmente un evento con la información
-  // que recibiría el componente de React
-  // fireEvent.change(inputMillas, { target: { value: '10' } })
-  // una variante más declarativa es
-  await userEvent.type(inputMillas, '10') // OJO que necesitamos el await!!
+  await userEvent.type(inputMillas, '10')
   // https://stackoverflow.com/questions/52618569/set-the-locale-for-date-prototype-tolocalestring-for-jest-tests
   expect(kilometers()).toBe('16,093')
 })
