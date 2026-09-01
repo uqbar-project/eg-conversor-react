ea294f125301b25aac7a257f66c9272fa8a086f4
fdodino <fernando.dodino@gmail.com>
2026-09-01 20:19:32 -0300

    Configuring single quotes completion + add describe

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import App from './App'
 
-test('convierte un valor > 0 de millas a kilómetros correctamente', async () => {
-  render(<App />)
-  const inputMillas = screen.getByTestId('millas')
-  await userEvent.type(inputMillas, '10')
-  // https://stackoverflow.com/questions/52618569/set-the-locale-for-date-prototype-tolocalestring-for-jest-tests
-  expect(kilometers()).toBe('16,093')
-})
-
-test('inicialmente pide que convirtamos de millas a kilómetros', () => {
-  render(<App />)
-  expect(kilometers()).toBe('<Ingrese millas>')
-})
-
 const kilometers = () => screen.getByTestId('kms').textContent
+
+describe('tests del conversor', () => {
+
+  test('convierte un valor > 0 de millas a kilómetros correctamente', async () => {
+    render(<App />)
+    const inputMillas = screen.getByTestId('millas')
+    await userEvent.type(inputMillas, '10')
+    expect(kilometers()).toBe('16,093')
+  })
+  
+  test('inicialmente pide que convirtamos de millas a kilómetros', () => {
+    render(<App />)
+    expect(kilometers()).toBe('<Ingrese millas>')
+  })
+
+})
