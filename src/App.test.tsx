fcc695062bf9dc2837f0093d7cc3f0f1494456a3
fdodino <fernando.dodino@gmail.com>
2026-09-03 23:18:34 -0300

    Linter fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,17 +6,15 @@ import App from './App'
 const kilometers = () => screen.getByTestId('kms').textContent
 
 describe('tests del conversor', () => {
-
   test('convierte un valor > 0 de millas a kilómetros correctamente', async () => {
     render(<App />)
     const inputMillas = screen.getByTestId('millas')
     await userEvent.type(inputMillas, '10')
     expect(kilometers()).toBe('16,093')
   })
-  
+
   test('inicialmente pide que convirtamos de millas a kilómetros', () => {
     render(<App />)
     expect(kilometers()).toBe('<Ingrese millas>')
   })
-
 })
